84db85f0dcf3ed2500c0080578704088106b6a3f
Pablo Quezado <83846001+pabloquezado@users.noreply.github.com>
2024-06-16 19:29:15 -0300

    Update serviceworker.js

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,19 +1,39 @@
-var staticCacheName = "pwa";
+const CACHE_NAME = 'cool-cache';
 
-self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"]);
-	})
-);
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/aulas/',
+  '/projetos/',
+  /sfx/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
-self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
